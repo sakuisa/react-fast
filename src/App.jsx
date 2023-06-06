091226e1bff2bab1ba70a4d3c062c94532c660d7
sakusan <sssaku.039@gmail.com>
2023-06-06 09:21:05 +0000

    Update App.jsx and ColorfulMessage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import ColofulMessage from './components/ColorfulMessage';
-
+import React, { useState } from "react";
+import ColofulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickCountUp = () => {
-  setNum(num + 1);
+    setNum(num + 1);
   };
-  const[num, setNum] = useSttate(0);
+  const [num, setNum] = useState(0);
   return (
     <>
-      <h1 style={{ color: "red" }}>こんにちは</h1>
-     <ColofulMessage color="blue" >お元気ですか？</ColofulMessage>
-      <ColofulMessage color="pink" >元気です！</ColofulMessage>
+      <h1 style={{ color: "green" }}>こんにちは</h1>
+      <ColofulMessage color="blue">お元気ですか？</ColofulMessage>
+      <ColofulMessage color="pink">元気です！</ColofulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
       <p>{num}</p>
       <p>_(:3 」∠)_</p>
